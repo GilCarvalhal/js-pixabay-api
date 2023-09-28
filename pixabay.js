51722db14aae01f326fe2074da7f66012764b526
Gil Carvalhal <gilbertocarvalhalmatos@gmail.com>
2023-09-27 21:17:51 -0300

    capturando o enter

--- pixabay.js
+++ pixabay.js
@@ -1,2 +1,15 @@
 "use strict";
 
+const loadGallery = (text) => {
+  console.log(text);
+};
+
+const handlerKeyPress = ({ key, target }) => {
+  if (key === "Enter") {
+    loadGallery(target.value);
+  }
+};
+
+document
+  .querySelector("#search-input")
+  .addEventListener("keypress", handlerKeyPress);
