02cb088392cd38b1f43f4239acd6257ff19070a1
Gil Carvalhal <gilbertocarvalhalmatos@gmail.com>
2023-09-30 00:05:29 -0300

    imagens pelas tags / paginacao

--- pixabay.js
+++ pixabay.js
@@ -7,54 +7,93 @@ const searchImages = async (text) => {
   return response.json();
 };
 
-const createLink = (tag) => {
-  `<a href="#">${tag}</a>`;
-};
+const createLink = (tag) => `
+    <a href="#" onClick="loadGallery('${tag}')">
+        ${tag}
+    </a>
+`;
 
 const createCard = ({ webformatURL, pageURL, tags, likes, comments }) => {
   const card = document.createElement("div");
   card.classList.add("card-container");
   card.innerHTML = `
-  <a href=${pageURL} class="card-image">
-  <img src=${webformatURL} />
-  <a />;
-  <div class="card-info">
-    <div class="card-tags">
-    ${tags.split(",").map(createLink).join("")}
-    </div>
+        <a href="${pageURL}" class="card-image">
+            <img src=${webformatURL} >
+        </a>
+        <div class="card-info">
+            <div class="card-tags">
+               ${tags.split(",").map(createLink).join("")}
+            </div>
+            <div class="card-action">  
+                <div class="card-like">
+                    <i class="far fa-thumbs-up"></i>
+                    <span>${likes}</span>
+                </div>
+                <div class="card-comment">
+                    <i class="far fa-comment"></i>
+                    <span>${comments}</span>
+                </div>
+                <div class="card-save">
+                    <i class="far fa-bookmark"></i>
+                </div>
+            </div>
 
-    <div class="card-action">
-    <div class="card-like">
-    <i class="fa-regular fa-thumbs-up"></i>
-    <span>${likes}</span>
-    </div>
-    <div class="card-comment">
-    <i class="fa-regular fa-comment"></i>
-    <span>${comments}</span>
-    </div>
-    <div class="card-save">
-    <i class="fa-regular fa-bookmark"></i>
-    </div>
-    </div>
-  </div>
-  `;
+        </div>
+    `;
   return card;
 };
 
-const loadGallery = async (text) => {
+const loadGallery = async (text, page = 1) => {
   const container = document.querySelector(".container-gallery");
-  const { hits } = await searchImages(text);
+  const { hits, totalHits } = await searchImages(`${text}&page=${page}`);
   const cards = hits.map(createCard);
   container.replaceChildren(...cards);
-  console.log(cards);
+
+  const totaPages = Math.ceil(totalHits / 20);
+  document.querySelector("#page-total").textContent = `/ ${totaPages}`;
+  document.querySelector("#search-input").value = text;
+  document.querySelector("#page").value = page;
 };
 
-const handlerKeyPress = ({ key, target }) => {
+const handleKeypress = ({ key, target }) => {
   if (key === "Enter") {
     loadGallery(target.value);
   }
 };
 
+const handlePage = ({ key, target }) => {
+  const text = document.querySelector("#search-input").value;
+  if (key === "Enter") {
+    loadGallery(text, target.value);
+  }
+};
+
+const handleNext = () => {
+  let page = Number(document.querySelector("#page").value);
+  const totalPages = Number(
+    document.querySelector("#page-total").textContent.replace("/", "")
+  );
+  const text = document.querySelector("#search-input").value;
+  if (page < totalPages) {
+    page++;
+    loadGallery(text, page);
+  }
+};
+
+const handlePrevious = () => {
+  let page = Number(document.querySelector("#page").value);
+  const text = document.querySelector("#search-input").value;
+  if (page > 1) {
+    page--;
+    loadGallery(text, page);
+  }
+};
+
 document
   .querySelector("#search-input")
-  .addEventListener("keypress", handlerKeyPress);
+  .addEventListener("keypress", handleKeypress);
+document.querySelector("#page").addEventListener("keypress", handlePage);
+document.querySelector("#page-next").addEventListener("click", handleNext);
+document
+  .querySelector("#page-previous")
+  .addEventListener("click", handlePrevious);
